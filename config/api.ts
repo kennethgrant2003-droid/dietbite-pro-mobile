8e3d63baa787cdece07a3384c8e37faf0c3313ac
Kenneth Grant <your-email@example.com>
2026-01-12 21:31:47 -0500

    Add expo-updates for EAS Update

--- config/api.ts
+++ config/api.ts
@@ -1,3 +1,4 @@
-// config/api.ts
+const PROD_URL = "https://dietbite-pro-backend-new.onrender.com";
+const DEV_URL = "http://192.168.1.199:3000";
 
-export const API_BASE_URL = "https://dietbite-pro-backend-1.onrender.com";
+export const API_BASE_URL = __DEV__ ? DEV_URL : PROD_URL;
